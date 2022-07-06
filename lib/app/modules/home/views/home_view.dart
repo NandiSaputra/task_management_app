cdbc065e9633a57d59f5bf560889a09b7c00f61c
Nandi Saputra <nandysyahputra59@gmail.com>
2022-07-06 18:27:08 +0700

    commit video 3

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -1,24 +1,29 @@
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
-
+import 'package:ionicons/ionicons.dart';
+import 'package:task_management_app/app/routes/app_pages.dart';
+import 'package:task_management_app/app/utils/widget/sidebar.dart';
 import '../controllers/home_controller.dart';
 
 class HomeView extends GetView<HomeController> {
-  const HomeView({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('HomeView'),
-        centerTitle: true,
-      ),
-      body: Center(
-        child: Text(
-          'HomeView is working',
-          style: TextStyle(fontSize: 20),
-        ),
-      ),
-    );
+        backgroundColor: Colors.blue[100],
+        body: Row(
+          children: [
+            Expanded(
+              flex: 2,
+              child: SideBar(),
+            ),
+            Expanded(
+              flex: 15,
+              child: Container(
+                color: Colors.white,
+              ),
+            )
+          ],
+        ));
   }
 }
