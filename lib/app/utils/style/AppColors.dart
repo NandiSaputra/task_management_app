c74e86f34c95b36d86d658c3777edc9936312cbb
Nandi Saputra <nandysyahputra59@gmail.com>
2022-07-06 22:34:56 +0700

    commit

--- lib/app/utils/style/AppColors.dart
+++ lib/app/utils/style/AppColors.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
-  static const primaryBg = Color.fromARGB(255, 69, 72, 75);
+  static const primaryBg = Color.fromARGB(255, 51, 75, 214);
   static const primaryText = Color.fromARGB(255, 62, 55, 55);
 }
